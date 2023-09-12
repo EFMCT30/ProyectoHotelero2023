7487b516b6e543a9afdb5e04349abacc4d87896b
Cdiazf <cdiazfarfan@gmail.com>
2023-09-12 16:10:12 -0500

    Creacion de entidades(Cliente, Habitaicon, Hotel, Reserva,ReservaHabitacion) aun falta definir las clases restantes  con los atributos .

---
+++ src/main/java/pe/edu/cibertec/ProyectoHotelero/entity/Habitacion.java
@@ -0,0 +1,36 @@
+package pe.edu.cibertec.ProyectoHotelero.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "habitaciones")
+public class Habitacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "habitacion_id")
+    private Long habitacionId;
+    private int numeroHabitacion;
+    private String tipo;
+    private int capacidad;
+    private BigDecimal precioNoche;
+    private boolean disponible;
+    @Column(name = "fecha_ultima_mantenimiento")
+    private Date fechaUltimaMantenimiento;
+    @ManyToOne
+    @JoinColumn(name = "hotel_id")
+    private Hotel hotel;
+    @OneToMany(mappedBy = "habitacion")
+    private List<ReservaHabitacion> reservaHabitaciones;
+}
+
