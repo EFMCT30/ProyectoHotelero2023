cc9ec41b4082e50588204e521d243e5a756f15da
EFMCT30 <enzofranchesco.ec@gmail.com>
2023-09-11 19:37:09 -0500

    initial commit

---
+++ src/test/java/pe/edu/cibertec/ProyectoHotelero/ProyectoHoteleroApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.ProyectoHotelero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoHoteleroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
