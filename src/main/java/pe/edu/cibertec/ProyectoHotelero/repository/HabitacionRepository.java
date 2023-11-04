c04b2595814bcfd8582792852f81c3109dc6bbd6
Gabomou07 <gabriel.vegabalcazar@gmail.com>
2023-11-03 21:48:33 -0500

    "Consultar por habitaciones disponibles"

--- src/main/java/pe/edu/cibertec/ProyectoHotelero/repository/HabitacionRepository.java
+++ src/main/java/pe/edu/cibertec/ProyectoHotelero/repository/HabitacionRepository.java
@@ -1,9 +1,15 @@
 package pe.edu.cibertec.ProyectoHotelero.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pe.edu.cibertec.ProyectoHotelero.entity.Habitacion;
 
+import java.util.List;
+
 @Repository
 public interface HabitacionRepository extends JpaRepository<Habitacion,Long> {
+
+    @Query("SELECT h FROM Habitacion h WHERE h.disponible = true")
+    List<Habitacion> findByDisponibleIsTrue();
 }
