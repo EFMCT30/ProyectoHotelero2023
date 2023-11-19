5b0d4cfc3f620b2a3db1a3d14fe01b02ace84519
EFMCT30 <enzofranchesco.ec@gmail.com>
2023-11-18 20:45:36 -0500

    Creación de GetUserName para obtener el usuario Logeado.

--- src/main/java/pe/edu/cibertec/ProyectoHotelero/controller/PrincipalController.java
+++ src/main/java/pe/edu/cibertec/ProyectoHotelero/controller/PrincipalController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -136,4 +138,12 @@ public class PrincipalController {
         userRepository.deleteById(Long.parseLong(id));
         return "Se ha borrado el user con id".concat(id);
     }
+
+    @GetMapping("/getUsername")
+    public ResponseEntity<String> getUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        return ResponseEntity.ok(username);
+    }
+
 }
